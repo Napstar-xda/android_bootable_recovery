5841defb628651676eb1e07c41062dd2b7d4f373
Napstar <xda.napstar@gmail.com>
2012-12-20 21:56:53 +0530

    Added on-screen touch navigation controls for CWM recovery menu
    Change-Id: Ie97c1d7f133b1ea636096deb24b889405d3b6c9f

--- extendedcommands.c
+++ extendedcommands.c
@@ -41,6 +41,7 @@
 #include "bmlutils/bmlutils.h"
 #include "cutils/android_reboot.h"
 
+#define ABS_MT_POSITION_X 0x35  /* Center X ellipse position */
 
 int signature_check_enabled = 1;
 int script_assert_enabled = 1;
@@ -1344,13 +1345,36 @@ void show_advanced_menu()
             {
                 ui_print("Outputting key codes.\n");
                 ui_print("Go back to end debugging.\n");
-                int key;
+                struct keyStruct{
+					int code;
+					int x;
+					int y;
+					int length;
+					int Xlength;
+				}*key;
                 int action;
                 do
                 {
                     key = ui_wait_key();
-                    action = device_handle_key(key, 1);
-                    ui_print("Key: %d\n", key);
+					if(key->code == ABS_MT_POSITION_X)
+					{
+				        action = device_handle_mouse(key, 1);
+						ui_print("Touch: X: %d\tY: %d\n", key->x, key->y);
+					}
+					else if(key->code == KEY_SCROLLDOWN)			//Gesture inputs: Helpful to come out of key test for devices with less hardware keys
+					{
+						action = GO_BACK;
+						ui_print("Gesture Touch: (KEY_SCROLLDOWN) Code: %d\n", key->code);
+					}
+					else if(key->code == KEY_SCROLLUP)
+					{
+						ui_print("Gesture Touch: (KEY_SCROLLUP) Code: %d\n", key->code);
+					}
+					else
+					{
+				        action = device_handle_key(key->code, 1);
+						ui_print("Key: %x\n", key->code);
+					}
                 }
                 while (action != GO_BACK);
                 break;
